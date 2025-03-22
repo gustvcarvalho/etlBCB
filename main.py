8d167127ee8d8d83c39cc7bac65cae1057f55b4e
Gustavo <gustavoferrazcarvalho@gmail.com>
2025-03-22 13:54:37 -0300

    primeiro commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def requestApiBcb(data):
     Parâmetros:
     data - string - aaaat (Exemplo: 20191)
     """
-    url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre=%27{data}&$format=json"
+    url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre=%27{data}%27&$format=json"
    
     req = requests.get(url) 
     #print(req.status_code)
