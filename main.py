178f1686e8a3d12d55059c5aa26241724719d11f
Gustavo Carvalho <164582165+gustvcarvalho@users.noreply.github.com>
2025-06-03 18:04:20 -0300

    versão final 1.0

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ dadosBcb = requestApiBcb("20191")
 
 #print(dadosBcb) #Exibe o DataFrame retornado pela API do Banco Central (extraxão e transformação)
 
-#salvarCsv(dadosBcb, "src/datasets/meiosPagamentosTri.csv", ";", ".") #Salva o DataFrame em um arquivo CSV;
+salvarCsv(dadosBcb, "src/datasets/meiosPagamentosTri.csv", ";", ".") #Salva o DataFrame em um arquivo CSV;
 
-salvarSQLite(dadosBcb, "src/datasets/etlbcb.db", "meios_pagamentos_tri") #Salva o DataFrame em um banco de dados SQLite;
+#salvarSQLite(dadosBcb, "src/datasets/etlbcb.db", "meios_pagamentos_tri") #Salva o DataFrame em um banco de dados SQLite;
 
 #salvarMySQL(dadosBcb, "root", "root", "localhost", "etlbcb", "meios_pagamentos_tri")
